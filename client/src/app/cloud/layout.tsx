bb0acb643a217e4d603dab7485ce001ae3ce4710
Vyacheslav <101479422+Juryxa@users.noreply.github.com>
2025-04-10 17:08:20 +0300

    clear trash and mobx add

--- client/src/app/cloud/layout.tsx
+++ client/src/app/cloud/layout.tsx
@@ -3,33 +3,30 @@
 import SideBar from '@/app/ui/SideBar';
 import React from 'react';
 import Header from '../ui/Header';
+import {observer} from "mobx-react-lite";
 
 
+function Layout({ children }: { children: React.ReactNode }) {
 
-
-
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  
   return (
-    
+
     <div className="flex h-screen h-[90vh] flex-col md:flex-row md:overflow-hidden ">
-       
+
       <div className="w-full flex-none md:w-64 bg-gray-100">
         <SideBar />
       </div>
-      
+
       <div className='flex flex-col flex-grow w-100%'>
         <Header/>
-        
-      
+
+
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
       </div>
     </div>
-    
+
   );
 }
-
+export default observer(Layout);
 
 
 
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,17 +1,13 @@
 'use client'
-import Link from "next/link";
-import { useContext, useState, useEffect } from "react";
-import UserService from "@/app/api/services/UserServices";
-import { Context } from "@/app/_app";
+import {useContext, useEffect} from "react";
+import {Context} from "@/app/_app";
 import LoginForm from "./api/components/LoginForm";
-import { IUser } from '@/app/api/models/IUser'
-import { observer } from "mobx-react-lite";
-import { useRouter } from 'next/navigation';
+import {observer} from "mobx-react-lite";
+import {useRouter} from 'next/navigation';
 
 function Home() {
   const { store } = useContext(Context);
   const router = useRouter(); // ✅ выносим вверх, чтобы вызывался всегда
-  const [users, setUsers] = useState<IUser[]>([]);
 
   useEffect(() => {
     if (localStorage.getItem('token')) {
@@ -25,14 +21,6 @@ function Home() {
     }
   }, [store.isAuth]);
 
-  async function getUsers() {
-    try {
-      const response = await UserService.fetchUsers();
-      setUsers(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  }
 
   if (store.isLoading) {
     return <div>Загрузка...</div>
@@ -42,7 +30,6 @@ function Home() {
     return (
       <div>
         <LoginForm />
-        <button onClick={getUsers}>Получить пользователей</button>
       </div>
     );
   }
