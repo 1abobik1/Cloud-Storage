a0ddc3d7f38bfe422ed9652542a98f6722fc4be4
mannco1 <f.mannco@yandex.ru>
2025-04-12 13:41:00 +0300

    fileSystem

--- client/src/app/components/TypeBlock.tsx
+++ client/src/app/components/TypeBlock.tsx
@@ -1,12 +1,11 @@
 'use client'
 import { useEffect, useState } from "react";
-
-
-import PhotoCard from '@/app/ui/PhotoCard';
 import { FileData } from "@/app/api/models/FileData";
 import CloudService from "../api/services/CloudServices";
 import FileUploader from "./FileUploader";
-export default function TypeBlock() {
+import FileCard from "@/app/ui/PhotoCard";
+
+export default function TypeBlock({type}) {
   const [file, setFile] = useState<FileData[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isError, setIsError] = useState<boolean>(false);
@@ -16,25 +15,34 @@ export default function TypeBlock() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await CloudService.getAllCloud('photo');
-        
-        const files: FileData[] = response.data.file_data.map((file: any) => ({
-          obj_id: String(file.obj_id),  
-          name: String(file.name),             
-          url: String(file.url), 
-          created_at:String(file.created_at)             
-        }));
-        setFile(files);
+        const response = await CloudService.getAllCloud(type);
+  
+        const fileData = response.data.file_data;
+  
+        if (Array.isArray(fileData)) {
+          const files: FileData[] = fileData.map((file: any) => ({
+            obj_id: String(file.obj_id),
+            name: String(file.name),
+            url: String(file.url),
+            created_at: String(file.created_at),
+          }));
+          setFile(files);
+        } else {
+          console.warn("file_data не является массивом:", fileData);
+          setFile([]); // или можно показать сообщение пользователю
+        }
+  
       } catch (error) {
         console.error("Ошибка при получении данных:", error);
-        setIsError(true); 
+        setIsError(true);
       } finally {
-        setIsLoading(false); 
+        setIsLoading(false);
       }
     };
-
+  
     fetchData();
-  }, []); 
+  }, []);
+  
 
   if (isLoading) return <p>Загрузка...</p>;
   if (isError) return <p>Произошла ошибка при загрузке данных.</p>;
@@ -45,11 +53,18 @@ export default function TypeBlock() {
   
   return (
     <div className="p-4 max-w-md mx-auto bg-white rounded shadow">
-      <h2 className="text-xl font-bold mb-4">Загрузка файла</h2>
-<FileUploader/>
-         {file.map((item) => (
-  <PhotoCard obj_id={item.obj_id} name={item.name} url={item.url} created_at={item.created_at} />
+      <h2 className="text-xl font-bold mb-4">Тип фалов: {type}</h2>
+      
+      {file.map((item) => (
+  <FileCard
+    key={item.obj_id} 
+    obj_id={item.obj_id}
+    name={item.name}
+    url={item.url}
+    created_at={item.created_at}
+  />
 ))}
+
     </div>
   );
 };
